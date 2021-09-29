9c137596a7c6b117e13a854a271287f09d2049de
sudhanshu <sudhanshus883@gmail.com>
2021-09-29 18:03:49 +0530

    changes the app to application

--- market/__init__.py
+++ market/__init__.py
@@ -3,7 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
-app = Flask(__name__)
+application = Flask(__name__)
+app=application
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
 app.config['SECRET_KEY'] = 'ec9439cfc6c796ae2029594d'
 db = SQLAlchemy(app)
